0d3c69fee6ad1a0121a68ad1ef7143ab4a79dd7d
borysSynowec <borys.synowec@gmail.com>
2020-03-10 22:47:02 +0100

    finished begginer functions

--- js/exercises_day1/2_functions.js
+++ js/exercises_day1/2_functions.js
@@ -1,8 +1,11 @@
 /*
 // TODO: 1. Napisz funkcję, która wyświetla console.log z dowolnym tekstem*/
 
-
-
+const Lake = () =>{ 
+let tatraLake = 'Morskie Oko';
+return tatraLake
+}
+console.log(Lake());
 
 
 /* TODO: 2. Napisz funkcję, która przyjmuje dwa parametry: firstName oraz lastName a następnie:
@@ -10,20 +13,20 @@
     - wynik funkcji przypisz do zmiennej o nazwie greeting
     - zadeklaruj funkcję jako function declaration oraz jako function expression */
 
-
-
+function GreetingSomebody(firstName,lastName){
+  let greeting = 'Hello'
+  return greeting +" "+firstName+" "+lastName+"!"
+}
+console.log(GreetingSomebody('Denis','Urubko'));
 
 /* TODO: 3. Napisz funkcję, która przyjmuje dwa parametry: firstName oraz lastName. Następnie:
     - zwraca stringa z powitaniem (wykorzystaj z funkcję z TODO1!) a do powitania
       dokleja stringa "I love you!"*
     - oczekiwana zwrotka: "Hello my dear firstName lastName! I love you!" */
 
-
-
-
-// TODO: 4. Opakuj kod z tego pliku w IIFE.
-
-// var greeting = function(firstName , lastName) {
-//     return 'hello my dear' + firstName + " " + lastName;
-// }
-
+const GreetingSomebody2 = (firstName,lastName) => {
+  let greeting = 'Hello';
+  let ending = 'you are cool';
+  return greeting+" "+firstName+" "+lastName+" "+ending+"!"
+}
+console.log(GreetingSomebody2('George','Mallory'));
