5695aa256292203d42dd152e13b04df1b9799a5c
borysSynowec <borys.synowec@gmail.com>
2020-03-10 22:28:10 +0100

    beginner varaibles finished

--- js/exercises_day1/1_variables.js
+++ js/exercises_day1/1_variables.js
@@ -1,29 +1,50 @@
 3
 // TODO: 1. Podepnij ten plik do pliku index.html
 
+
 // TODO: 2. Zadeklaruj 2 zmienne typu string
 
+let peak = 'kasprowy'
+let peakPopularity = 'huge'
+console.log(peak,peakPopularity);
+
+
 // TODO: 3. Zadeklaruj 2 zmienne typu number
+
+let peakHeight = 2300;
+let peakAbsoluteHeight = 800;
+console.log(peakHeight, peakAbsoluteHeight);
 
 
 // TODO: 4. Zadeklaruj 2 zmienne typu boolean
 
+let ISpeakIced = false;
+let iSpeakRocky = true;
+console.log(ISpeakIced,iSpeakRocky);
 
 
 // TODO: 5. Zadeklaruj zmienna typu undefined
 
+let tatras 
+console.log(tatras);
 
 
 // TODO: 6. Zadeklaruj zmienna typu null
 
+let mySallary = null
+console.log(mySallary);
 
 
 // TODO: 7. Zadeklaruj tablicę zawierajacą zmienne typu string
 
+HighTatraPeaks = ['mnich','rysy','zabi'];
+console.log(HighTatraPeaks)
 
 
 // TODO: 8. Zadeklaruj tablicę zawierajacą zmienne różnego typu
 
+MountRysy = [ 2501, 'Polish-Slovak']
+console.log(MountRysy);
 
 
 // TODO: 9. Zadeklaruj obiekt o nazwie peak zawierajacy pola:
@@ -32,11 +53,16 @@
 //  - dificulty
 //  - country
 
+let peakObject = {
+    name: 'Zabi',
+    height: 2126,
+    dificulty: 'hard',
+    country: "'Poland''Slovakia"
+}
+console.log(peakObject);
 
 
 // TODO: 10. Sprawdź typ co najmniej jeden zmiennej z poprzednich zadań
 
-
-
-// TODO: 11. Nadpisz wartość dowolnej zmiennej zadeklarownej w poprzednim zadaniu
+console.log(typeof peakObject);
 
