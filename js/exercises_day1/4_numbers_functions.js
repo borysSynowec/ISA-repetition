3f26e012d162c811fc637930276a2d83b5cc5700
borysSynowec <borys.synowec@gmail.com>
2020-03-11 01:24:57 +0100

    basic number fuctions finished

--- js/exercises_day1/4_numbers_functions.js
+++ js/exercises_day1/4_numbers_functions.js
@@ -7,17 +7,61 @@ Wskazówki do zadań:
 - val.toString() - konwersja number do string */
 
 // TODO: 1. Napisz funkcję, która pobiera liczbę i zwraca ją skonwertowaną na string
+// ver1
+const convertToString = (Number) => {
+    return Number.toString();
+}
+console.log(convertToString(20));
+
+// ver2
+const convertToString2 = (Number) => {
+    let string = Number.toString();
+    return string;
+}
+console.log(convertToString2(20));
+
+// ver3
+const convertToString3 = (Number) => {
+    return Number.toString();
+}
+
+// ver4
+// const convertToString4 =()=>{
+
+//     const numberToStringChild =(Number)=>{
+//     return Number.toString();
+// }
+// let brandNewString = numberToStringChild();
+// }
+// convertToString4();
+
 
 
 // TODO: 2. Napisz funkcję, która pobiera stringa i zwraca go skonwertowanego na number
+
+const StringToNumber = (str) => {
+    return parseFloat(str);
+}
 
 
 /* TODO: 3. W poniższych zmiennych, doprecyzuj rozwinięcie dziesiętne. Nadpisz wartość zmiennych nową wartością.
     - var powerHorses = 233; do 3 liczb po przecinku
     - var score = 10.33344; do 2 liczb po przecinku */
-   
 
+    const numberFixed = () =>{
+        let powerHorses = 233
+        return powerHorses.toFixed(3);
+    }
+    console.log(numberFixed());
+
+    const numberFixed2 = ( ) =>{
+        let score = 10.33344
+        return score.toFixed(2);
+    }
+    console.log(numberFixed2());
 
     // bez zadeklarowanego 
 
-    
+const numberFixedUndeclared = (num) =>{
+    return num.toFixed(2);
+};
