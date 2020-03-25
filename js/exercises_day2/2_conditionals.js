6cc616788c8465346c1ef5eff6e4a069a0c5b208
borysSynowec <borys.synowec@gmail.com>
2020-03-25 21:49:22 +0100

    conditional basic exercises finished

--- js/exercises_day2/2_conditionals.js
+++ js/exercises_day2/2_conditionals.js
@@ -4,6 +4,12 @@
  - a jeśli nie jest, to wyświetli ("Jednak nie junior dev:( ");
  */
 
+let developer = 'junior js dev'
+if (developer === 'junior js dev') {
+    console.log('hura jestem junior devem')
+} else {
+    console.log('jednak nie')
+}
 
 
 /* TODO.2: Napisz instrukcję IF / ELSE IF / ELSE, która:
@@ -12,52 +18,63 @@
     - wyświetli console.log("Czas na emeryturę!") jeśli Twój wiek jest <= 33 i > 18
     - wyświetli console.log("Hej młodziaku!") jeśli Twój wiek jest <= 18
  */
-
+const ageFunction = age => {
+    if (age >= 67) {
+        console.log('emerytura')
+    } else if (age < 67 && age >= 21) {
+        console.log('sredni wiek')
+    } else {
+        console.log('ty dzieciorze')
+    }
+}
 
 
 /* TODO.3: Napisz funkcję, która zwraca true jeśli:
     - wartość zmiennej skill jest równa 'javaScript' lub 'HTML' lub 'CSS'
  */
-
+const skillFunction = skill => {
+    if (skill === 'JS' || skill === 'HTML' || skill === 'CSS') {
+        return true
+    } else {
+        return false
+    };
+}
 
 /* TODO.4: Napisz funkcję, która przyjmuje jako parametr stringa i zwraca true jeśli:
  - wartość przyjętego parametru jest różna od słowa JavaScript
  */
-
+const stringCheck = string => {
+    if (string != 'JavaScript') {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 /* TODO.5: Napisz funkcję isShopOpen, która przyjmuje jako parametr liczbę i zwraca true jeśli:
  - wartość parametru jest mniejsza niż 23 i większa od 6
  */
-
-
+const isShopOpen = openHours => {
+    if (openHours > 6 && openHours < 23) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 /* TODO.6: Wykorzystując funkcję isShopOpen i negację (!), napisz funkcję isShopClosed
     - która zwraca true, jeśli sklep jest zamknięty (czyli neguje wartość zwróconą z isShopOpen)
  */
 
-
-
-/* TODO.7: Wykorzystując SWITCH, napisz funkcję, która jako parametr przyjmuje stringa o nazwie day i:
- - która wyrzuca w konsoli wymyślony tekst, w zależności czy "case" jest spełniony
- - case mają odpowiadać kolejnym dniom: "Monday", "Tuesday", "Wednesday" etc...
- - ma być parę przynajmniej przypadków (np. Monday, Tuesday, Saturday)
- - w przypadku jeśli case nie pasuje do żadnego przypadku, to funkcja ma wyświetlić w konsoli tekst: "Taki dzień nie istnieje!";
- */
-
-
-/* TODO.8: Wykorzystując TERNARY OPERATOR:
-    - przypisz do zmiennej price wartość: '2.00$' jeśli isMember = true a jeśli nie to '10.00$';
-        czy
-  */
-
-
-
-
-
-
-
+const isShopClosed = (closedHours) =>{
+    if (closedHours >! 6 && closedHours <! 23) {
+        return false
+    } else {
+        return true
+    }
+}
 
 
 
